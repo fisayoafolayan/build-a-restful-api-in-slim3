7e7a51808043aeb6bd58e084e161e632e519de51
Fisayo Afolayan <afolayanfisayo@gmail.com>
2018-08-11 00:18:53 +0100

    added live urls to homepage

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Controllers\HomeController;
 use App\Controllers\VoucherController;
 
+$app->get('/', HomeController::class . ':index');
 $app->post('/api/offers/create', VoucherController::class . ':createOffers');
 $app->post('/api/voucher/validate', VoucherController::class . ':validateVoucher');
 $app->get('/api/voucher/list', VoucherController::class . ':fetchAllValidVoucherPerUser');
