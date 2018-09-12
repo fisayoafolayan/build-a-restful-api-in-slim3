aeaaffc1dfd8700c18c337176efbe93bef360bcc
Fisayo Afolayan <afolayanfisayo@gmail.com>
2018-09-12 21:39:32 +0100

    changed from mysql to sqlite

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,11 +22,7 @@ $app = new Slim\App([
 
         'database' => [
             'driver'    => getenv('DB_DRIVER'),
-            'host'      => getenv('DB_HOST'),
-            'port'      => getenv('DB_PORT'),
-            'database'  => getenv('DB_DATABASE'),
-            'username'  => getenv('DB_USERNAME'),
-            'password'  => getenv('DB_PASSWORD')
+            'database'  => getenv('DB_DATABASE')
         ]
     ],
 ]);
